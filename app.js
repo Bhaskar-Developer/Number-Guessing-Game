9fd85a2a9bb873864c2366ff35f625b32f40eba4
Bhaskar-Developer <bhaskarchetty22@gmail.com>
2021-06-30 21:43:05 +0530

    added logic to handle winning condition, losing condition and continue playing condition

--- app.js
+++ app.js
@@ -36,23 +36,48 @@ guessBtn.addEventListener('click', () => {
   if(isNaN(guess) || guess < min || guess > max) {
     //Tell the player to enter valid input by showing a Message in the UI
     setMessage(`Please enter a number between ${min} and ${max}`,'red')
+    return
   } 
 
   //Check if Won i.e. the player has guessed the correct number
   if(guess===winningNumber) {
-    //disable the input and show the message on UI
-    guessInput.disabled = true;
-    //set the border color if guess input to green
-    guessInput.style.borderColor = 'green'
-    //Tell The Player that the guess was correct by showing a message on the UI.    
-    setMessage(`${guess} was the correct Guess! You Won`,'green')
+    //Game Over - Player has won
+    gameOver(true,`${winningNumber} is the correct guess. You Won!`) 
   } else {
-    //
+    //player loses
+    //wrong number guessed
+    //reduce the number of guesses
+    guessesLeft-=1;
+
+    //check if there is any guesses remaining
+
+    //if remaining guesses is 0 which means game over
+    if(guessesLeft === 0) {
+      //Game Over - Player Lost
+      gameOver(false,`Game Over. You Lost. The correct number was ${winningNumber}`);
+    } else {
+      //Game Not Over - Continue playing
+      gameOver(false,`Wrong guess. You have ${guessesLeft} guesses left`)
+    }
   }
 })
 
 function setMessage(msg,color) {
   message.textContent=msg
   message.style.color=color
+}
+
+function gameOver(won,message) {
+  let color;
+  //using ternary operator to set the value of color.
+  //If won=true then color will be green else color will be red
+  won === true ? color = 'green' : color = 'red'
+  
+  //disable the input and show the message on UI
+  guessInput.disabled = true;
+  //set the border color based on lost or won
+  guessInput.style.borderColor = color
+  //Tell The Player that the guess was correct by showing a message on the UI.    
+  setMessage(message,color)
 }
       
