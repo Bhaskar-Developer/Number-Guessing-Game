61ffc0cc64bacf708c7cb6a616aaff8d207bb5e6
Bhaskar-Developer <bhaskarchetty22@gmail.com>
2021-06-30 22:38:53 +0530

    Added logic for Play Again if the player wins or loses

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ Functions/Rules of the Game
 //Game values
 let min = 1,
     max = 10,
-    winningNumber = 2 //for now
+    winningNumber = setRandomNumber(min,max)
     guessesLeft = 3;
 
 //UI elements
@@ -25,6 +25,20 @@ const game = document.querySelector('.game'),
 //We assign the min and max that we defined to UI variables minNum and maxNum
 minNum.textContent=min
 maxNum.textContent=max 
+
+//Play Again Event Listener
+
+//mousedown even means only mouse push down
+//click = mousedown+mouseup
+//click will cause the window to flash and not display play again option on UI
+//Hence we listen for mousedown 
+guessBtn.addEventListener('mousedown',(e) => {
+  //check if the target we are listening to is the play again button
+  if(e.target.className === 'play-again') {
+    //reload the window once play again button is clicked
+    window.location.reload()
+  }
+})
 
 //Listen for Guess (submit button) 
 guessBtn.addEventListener('click', () => {
@@ -42,7 +56,11 @@ guessBtn.addEventListener('click', () => {
   //Check if Won i.e. the player has guessed the correct number
   if(guess===winningNumber) {
     //Game Over - Player has won
-    gameOver(true,`${winningNumber} is the correct guess. You Won!`) 
+    gameOver(true,`${winningNumber} is the correct guess. You Won!`)
+    
+    //Play Again
+    //Allow the player to play again
+    playAgain()
   } else {
     //player loses
     //wrong number guessed
@@ -55,6 +73,8 @@ guessBtn.addEventListener('click', () => {
     if(guessesLeft === 0) {
       //Game Over - Player Lost
       gameOver(false,`Game Over. You Lost. The correct number was ${winningNumber}`);
+      //Give the player a chance to play again if Lost
+      playAgain()
     } else {
       //Game Not Over - Continue playing
       gameOver(false,`Wrong guess. You have ${guessesLeft} guesses left`)
@@ -73,11 +93,28 @@ function gameOver(won,message) {
   //If won=true then color will be green else color will be red
   won === true ? color = 'green' : color = 'red'
   
-  //disable the input and show the message on UI
-  guessInput.disabled = true;
+  //disable the input if the remaining guesses are not zero
+  if(guessesLeft !== 0) {
+    guessInput.disabled = false;
+  } else { //disable the input if guesses becomes zero
+    guessInput.disabled = true;
+  }
   //set the border color based on lost or won
   guessInput.style.borderColor = color
   //Tell The Player that the guess was correct by showing a message on the UI.    
   setMessage(message,color)
 }
       
+function playAgain() {
+  //change the guess button text to Play Again
+  guessBtn.value = 'Play Again'
+  //add a class to the guess button. This will be used to target the button during event delegation where the window is reloaded
+  guessBtn.className += 'play-again'
+  //disable the guess input
+  guessInput.disabled = true
+}
+
+//generate Random number between min and max
+function setRandomNumber(min, max) {
+  return Math.floor((Math.random()*(max-min+1)+min))
+}
