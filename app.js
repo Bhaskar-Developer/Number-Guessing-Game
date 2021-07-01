273fd77f73cb5468cd079e93564ed7ed1db37c90
Bhaskar-Developer <bhaskarchetty22@gmail.com>
2021-07-01 13:51:15 +0530

    Comments Added at relevant places

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 /*
 Functions/Rules of the Game
--Player must gues a number between Min and the Max
--Player gets a certain amount of guesses
--Notify player of guesses remaining
--Notify the player of the correct answer if loose
--Let play choose to play again
+  -Player must gues a number between Min and the Max
+  -Player gets a certain amount of guesses
+  -Notify player of guesses remaining
+  -Notify the player of the correct answer if loose
+  -Let play choose to play again
 */
 
 //Game values
 let min = 1,
     max = 10,
-    winningNumber = setRandomNumber(min,max)
-    guessesLeft = 3;
+    winningNumber = setRandomNumber(min,max) //This function is used to generate a random number between 1 an d 10
+    guessesLeft = 3; //We give the player with 3 guesses
 
 //UI elements
 const game = document.querySelector('.game'),
@@ -30,12 +30,12 @@ maxNum.textContent=max
 
 //mousedown even means only mouse push down
 //click = mousedown+mouseup
-//click will cause the window to flash and not display play again option on UI
-//Hence we listen for mousedown 
+//click will cause the window to flash and not display Play Again button on UI
+//Hence we listen for mousedown event 
 guessBtn.addEventListener('mousedown',(e) => {
   //check if the target we are listening to is the play again button
   if(e.target.className === 'play-again') {
-    //reload the window once play again button is clicked
+    //reload the window once Play Again button is clicked
     window.location.reload()
   }
 })
@@ -46,7 +46,8 @@ guessBtn.addEventListener('click', () => {
   //We parse the received input as a Number
   let guess = parseInt(guessInput.value)
   
-  //Validate the input to check if it is a number and also check if the entered number lies between min and max
+  //Validate the input guess input
+  //make sure it is a number and lies between min and max
   if(isNaN(guess) || guess < min || guess > max) {
     //Tell the player to enter valid input by showing a Message in the UI
     setMessage(`Please enter a number between ${min} and ${max}`,'red')
@@ -55,28 +56,25 @@ guessBtn.addEventListener('click', () => {
 
   //Check if Won i.e. the player has guessed the correct number
   if(guess===winningNumber) {
-    //Game Over - Player has won
+    //Game Over - Player has Won 
     gameOver(true,`${winningNumber} is the correct guess. You Won!`)
     
-    //Play Again
-    //Allow the player to play again
+    //Give an option for the player to play again
     playAgain()
   } else {
-    //player loses
     //wrong number guessed
     //reduce the number of guesses
     guessesLeft-=1;
 
     //check if there is any guesses remaining
-
-    //if remaining guesses is 0 which means game over
+    //if remaining guesses is 0 then its game over
     if(guessesLeft === 0) {
       //Game Over - Player Lost
       gameOver(false,`Game Over. You Lost. The correct number was ${winningNumber}`);
-      //Give the player a chance to play again if Lost
+      //Give the player an option to play again
       playAgain()
     } else {
-      //Game Not Over - Continue playing
+      //Game Not Over - Continue playing i.e. The are still guesses remaining
       gameOver(false,`Wrong guess. You have ${guessesLeft} guesses left`)
     }
   }
@@ -90,7 +88,7 @@ function setMessage(msg,color) {
 function gameOver(won,message) {
   let color;
   //using ternary operator to set the value of color.
-  //If won=true then color will be green else color will be red
+  //If won=true then color=green else color=red
   won === true ? color = 'green' : color = 'red'
   
   //disable the input if the remaining guesses are zero
@@ -101,20 +99,20 @@ function gameOver(won,message) {
   }
   //set the border color based on lost or won
   guessInput.style.borderColor = color
-  //Tell The Player that the guess was correct by showing a message on the UI.    
+  //Let the Player know that the guess was correct or wrong by showing a message on the UI.    
   setMessage(message,color)
 }
       
 function playAgain() {
   //change the guess button text to Play Again
   guessBtn.value = 'Play Again'
-  //add a class to the guess button. This will be used to target the button during event delegation where the window is reloaded
+  //append a class to the guess button. This will be used to target the button during event delegation where the window is reloaded
   guessBtn.className += 'play-again'
   //disable the guess input
   guessInput.disabled = true
 }
 
-//generate Random number between min and max
+//generate Random number between min and max. This also includes min and max
 function setRandomNumber(min, max) {
   return Math.floor((Math.random()*(max-min+1)+min))
 }
